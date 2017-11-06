47b97ddeec3035d4a9f21525ad678c11edeb54c8
psykrsna <saivnm5@gmail.com>
2017-11-07 02:17:59 +0530

    share url bug

--- src/stories/Publish.js
+++ src/stories/Publish.js
@@ -93,7 +93,7 @@ class Publish extends Component {
           data: data
         }).then(function(response){
             var data = response.data.data;
-            var shareUrl = getShareUrl(this.props.data.story, data.publishStory);
+            var shareUrl = getShareUrl(comp.props.data.story, data.publishStory);
             comp.setState({
                 shareUrl: shareUrl,
                 isPublished: true
