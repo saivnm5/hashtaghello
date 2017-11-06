7de01ed451fd9be5465de6cb07960277392ec3c2
psykrsna <saivnm5@gmail.com>
2017-11-07 02:38:48 +0530

    removing home, fuck that shit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
 
   render() {
     if(localStorage.getItem('isLoggedIn') === "true"){
-      return ( <Index /> );
+      return ( <Profile /> );
     }
     else{
       return ( <Landing /> );
